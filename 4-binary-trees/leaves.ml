8e78c4f7268ae1d8da3b67f67473bd79c2bd03ec
Andrew Elkouri <andrewelkouri@gmail.com>
2015-12-27 14:36:11 -0600

    added leaves to list function

---
+++ 4-binary-trees/leaves.ml
@@ -0,0 +1,21 @@
+type 'a binary_tree =
+    | Empty
+    | Node of 'a * 'a binary_tree * 'a binary_tree
+;;
+
+let leaves tree =
+    let rec leaves_aux acc = function
+        | Empty -> acc
+        | Node(n, Empty, Empty) -> n::acc
+        | Node(_, l, r) -> (leaves_aux acc l)@(leaves_aux acc r)
+    in
+    leaves_aux [] tree
+;;
+
+let example_tree =
+    Node('a', Node('b', Node('d', Empty, Empty), Node('e', Empty, Empty)),
+         Node('c', Empty, Node('f', Node('g', Empty, Empty), Empty)))
+;;
+
+assert (leaves Empty = []) ;;
+assert (leaves example_tree = ['d'; 'e'; 'g']) ;;
